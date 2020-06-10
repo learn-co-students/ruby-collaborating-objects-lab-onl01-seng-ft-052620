1a7415c89b4cb037893e9d5bb47f4b7ecd5c251b
Eyerusalem K <3yerus@gmail.com>
2020-06-10 18:25:06 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,6 +1,6 @@
 require "pry"
 class Artist 
-attr_accessor :name, :song, :all, :songs
+attr_accessor :name, :all, :songs
 
 @@all = []
 def initialize(name)
@@ -13,9 +13,37 @@ end
   end
   
   def add_song(song)
-    binding.pry
-  self.songs = song
+    # binding.pry
+  song.artist = self
   
+  end
+  
+  def self.find_or_create_by_name(name) 
+  if !self.find(name)
+    self.create(name)
+  else 
+    self.find(name)
+  end
+  end 
+  
+  
+  def self.find(name)
+    @@all.find do |artist|
+      artist.name == name
+    end
+  end
+
+  def self.create(name)
+    artist = self.new(name)
+   artist
+  end
+  
+  def print_songs
+ puts songs.collect {|song| song.name}
+  end
+  
+  def songs
+      Song.all.select {|song| song.artist == self }
   end
   
   
