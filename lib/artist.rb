da38be41d9fe8ea7c95411fd0d915cc258a38931
ejguzman3988 <eriberto176@gmail.com>
2020-06-11 02:26:21 -0400

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,46 @@
+class Artist
+    attr_accessor :name
+    @@all = []
+
+    def initialize(name)
+        # accepts a name for the artist
+        @name = name
+        save
+    end
+
+    def save
+        # saves each instance of an artist to an array called @@all
+        @@all << self
+    end
+
+    def self.all
+        # returns all existing Artist instances
+        @@all
+    end
+
+    def add_song(song)
+        #keeps track of an artist's songs
+        song.artist = self
+    end
+
+    def songs
+        Song.all.select{|song| song.artist == self}
+    end
+
+    def self.find_or_create_by_name(name) # => an instance of Artist
+        # finds or creates an artist by name maintaining uniqueness of objects name property
+        # Creates a new instance of Artist if none exist
+        
+        if !self.all.any?{|artist| artist.name == name}
+            self.new(name)
+        else
+            self.all.find{|artist| artist.name == name}
+        end
+
+    end
+
+    def print_songs
+        # lists all of the artist's songs
+        puts Song.all.select{|song| puts song.name if song.artist == self}
+    end
+end
